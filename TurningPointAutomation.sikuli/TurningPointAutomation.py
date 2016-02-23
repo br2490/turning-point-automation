58912857b4f8b0dc483c73d2daa84cf4731c690d
Ben Rosner <brosner@barnard.edu>
2016-02-22 22:45:18 -0500

    Modifying search method.

--- TurningPointAutomation.sikuli/TurningPointAutomation.py
+++ TurningPointAutomation.sikuli/TurningPointAutomation.py
@@ -1,23 +1,17 @@
 Debug.on(3)
-# Settings.ObserveScanRate = 0.1
-
 ## configure our patterns.
 # STEP 0, we need to open polling.
-poll_closed_no_response = Pattern("poll_closed_no_response.png").exact() 
 poll_closed_no_response_old = Pattern("poll_closed_no_response_old.png").exact()
-poll_closed_no_response_zoom = Pattern("poll_closed_no_response_zoom.png").exact()
 # STEP 2, we need to wait for a response.
-poll_opened_no_response = Pattern("poll_open_no_response.png").exact() 
 poll_open_no_response_old = Pattern("poll_open_no_response_old.png").exact()
-poll_open_no_response_zoom = Pattern("poll_open_no_response_zoom.png").exact()
 # STEP 3, we have a response, close polling.
-poll_opened_has_response = Pattern("poll_opened_has_response.png").exact() 
 poll_opened_has_response_old = Pattern("poll_opened_has_response_old.png").exact()
-poll_opened_has_response_zoom = Pattern("poll_opened_has_response_zoom.png").exact()
 # STEP 4 -> 1, saved response.
-poll_closed_has_response = Pattern("poll_closed_has_response.png").exact() 
 poll_closed_has_response_old = Pattern("poll_closed_has_response_old.png").exact()
-poll_closed_has_response_zoom = Pattern("poll_closed_has_response_zoom.png").exact()
+# ZERO
+no_response = Pattern("ZERO.png").exact()
+# ONE
+has_response = Pattern("ONE.png").exact()
 
 ## This hotkey will both OPEN and CLOSE polling on Mac OSx.
 def turning_point_hotkey():
@@ -27,28 +21,42 @@ def turning_point_hotkey():
 def next_slide_and_poll():
     print("next slide")
 
-## First let's find the TurningPoint polling toolbar. This MATCH is saved as our region.
-region = wait(poll_closed_no_response_zoom, FOREVER)
-# Increase the speed at which we poll the region for changes. Where scanRate(n) is equal to queries per second.
-region.setWaitScanRate(20)
+## First let's find the TurningPoint polling toolbar. This MATCH is saved as our search region.
+toolbar_region = wait(poll_closed_no_response_old, FOREVER)
 
+## Now we'll build a new region based on our first region that captures ONLY the number of responses (0 or 1)
+newX = toolbar_region.getX() + 395 # since we know how big the region is we add a fixed number of pixels to X
+newY = toolbar_region.getY() + 24 # since we know how big the region is we add a fixed number of pixels to Y
+h_and_w = 14 # the hieght and width of our textual element is 14px...
+text_region = setRect(newX, newY, h_and_w, h_and_w) ## This region is our end result.
+
+# Increase the speed at which we poll the region for changes. Where scanRate(n) is equal to queries per second.
+setWaitScanRate(20) # scan 50 TPS
+
+# DEBUG COMMANDS
+toolbar_region.highlight(2)
+text_region.highlight(2)
+Screen(0).highlight(2)
+#print(text_region.text())
+
+## Now that we've set all of our regions and "know" where to scan the screen, let's begin the experiment, shall we?!
 ## Open polling and wait for participant.
-turning_point_hotkey()
+# turning_point_hotkey()
 
 # if exists(poll_closed_no_response_old, 10):
 #     turning_point_hotkey()
 #     wait(1)
 
 # This is a SANITY check ~ we should se that polling is opened and waiting for input.
-region = region.wait(poll_open_no_response_zoom, 10)
+region.wait(poll_open_no_response_old, 10)
 print("Waiting for input...")
 
 # The participant has responded, wait, then close polling.
-region = region.wait(poll_opened_has_response_zoom, FOREVER)
+textRegion.wait(has_response, FOREVER)
 print("Participant has 1 second to change their answer")
-sleep(10) # give the participant opportunity to change their response.
+sleep(1) # give the participant opportunity to change their response.
 print("Participant no longer has time to change their answer.")
-turning_point_hotkey()
+#turning_point_hotkey()
 
-if region.wait(poll_closed_has_response_zoom):
-    print("WE HAVE A RESPONSE AND POLLING IS CLOSED!!!")
+#if region.wait(poll_closed_has_response_zoom):
+#    print("WE HAVE A RESPONSE AND POLLING IS CLOSED!!!")
